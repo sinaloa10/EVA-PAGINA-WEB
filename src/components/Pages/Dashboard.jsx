71ba33e39cb8f3e43760e97b14a4061cd7842308
sinaloa10 <oscarsinaloa47@gmail.com>
2025-05-19 08:41:43 -0600

    enviando a prod

--- src/components/Pages/Dashboard.jsx
+++ src/components/Pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import Calendar from "react-calendar";
 import './Dashboard.css';
 
-//SE MANEJARÁ DASHBOARD Y PÁGINAS RELACIONADAS EN PAGES
+//SE MANEJARÁ DASHBOARD Y EN PAGES
 
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import { useState, useEffect } from "react";
